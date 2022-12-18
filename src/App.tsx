558935a99628dbba2102a399c1c306dde63191d2
Yasser Shalash <yasservanshalash@gmail.com>
2022-12-18 12:34:49 +0100

    yummy boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Recipe from './pages/Recipe';
+import Favorite from './pages/Favorite';
+import Contact from './pages/Contact';
 import "./App.css";
 
 function App() {
