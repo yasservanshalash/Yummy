61caebde0489783e500a591aaf5465d00c40e876
Yasser Shalash <yasservanshalash@gmail.com>
2022-12-19 12:41:25 +0100

    added mui textField

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import "./Form.css"
+import React from "react";
+import "./Form.css";
+import TextField from "@mui/material/TextField";
 
 type PropType = {
-    setUserInput: Function;
-}
-const Form = ({setUserInput}: PropType) => {
-
-    const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setUserInput(e.target.value);
-    }
+  setUserInput: Function;
+};
+const Form = ({ setUserInput }: PropType) => {
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserInput(e.target.value);
+  };
 
   return (
     <div>
-        <input type="text" onChange={changeHandler} />
+      <form>
+        <TextField
+          id="outlined-helperText"
+          label="Please enter the name"
+          defaultValue="lamb"
+          helperText="suggestions: pizza, pork, pasta..."
+          onChange={changeHandler}
+        />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
